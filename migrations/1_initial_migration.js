2d2f77764ed1aeec50f3f67b2e8c8b6e94482321
Facundo Carballo <facundocarballo@MacBook-Pro-de-Facundo.local>
2022-01-25 21:03:45 -0300

    FacuTokenSale v1.0.2

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,11 @@
-const Migrations = artifacts.require("Migrations");
-const FacuToken = artifacts.require("FacuToken");
-const FacuTokenSale = artifacts.require("FacuTokenSale");
+const FacuToken = artifacts.require("./facuTokenICO/FacuToken");
+const FacuTokenSale = artifacts.require("./facuTokenICO/FacuTokenSale");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(FacuToken).then(() => 
-    deployer.deploy(FacuTokenSale, FacuToken.address)
-  );
+  deployer.deploy(FacuToken).then(() => {
+    deployer.deploy(FacuTokenSale, FacuToken.address);
+  });
+  // If this code doesn't work, to deploy the contract token sale
+  // we have to copy the contract tokenn address and paste here
+  // deployer.deploy(FacuTokenSale, "0xBC0484Ab69982738BcD5FA45947Fc9203551bB7c");
 };
